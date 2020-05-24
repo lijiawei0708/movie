750af7bbf4d6e82e550febac9eb99d8212fd26c9
Chris <lijiawei0708@gmail.com>
2020-05-24 21:01:07 +1000

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  movieListApi: 'https://api.themoviedb.org/3/discover/movie?',
+  movieInfoApi: 'https://api.themoviedb.org/3/movie/',
+  movieSearchApi: 'https://api.themoviedb.org/3/search/movie?',
+  apiKey: '6ed12e064b90ae1290fa326ce9e790ff',
+  imgURL: 'https://image.tmdb.org/t/p/w500'
 };
